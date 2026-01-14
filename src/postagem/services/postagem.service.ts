1016b66175d5f09ef06b6b90a7b4f51569055bf1
Matheus Lins <matheusantonioslins@outlook.com>
2026-01-13 23:13:56 -0300

    Implementação dos Métodos

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ILike, Repository, DeleteResult } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
 
 @Injectable()
@@ -15,8 +15,44 @@ export class PostagemService {
   }
 
   //get por ID
-  async findById(id:number): Promise<Postagem> {
-    const postagem = await this.postagemRepository.findOne({where:{id}});
-    
+  async findById(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
+    if (!postagem)
+      throw new HttpException('Postagem nao encontrada!', HttpStatus.NOT_FOUND);
+
+    return postagem;
+  }
+
+  //get por Título
+  async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`),
+      },
+    });
+  }
+
+  //post
+  async create(postagem: Postagem): Promise<Postagem> {
+    return await this.postagemRepository.save(postagem);
+  }
+
+  //update
+  async update(postagem: Postagem): Promise<Postagem> {
+    await this.findById(postagem.id);
+
+    return await this.postagemRepository.save(postagem);
+  }
+
+  //delete
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+
+    return await this.postagemRepository.delete(id);
+  }
 }

68fc443f977ba6d3be2d3e09b430e8012310aedf
Matheus Lins <matheusantonioslins@outlook.com>
2026-01-15 23:22:00 -0300

    Relacionamento de classes postagem e tema

--- src/Tema/tema.module.ts
+++ src/Tema/tema.module.ts
@@ -1,11 +1,11 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Tema } from "./entities/tema.entity";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tema } from './entities/tema.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Tema])],
-    providers: [],
-    controllers: [],
-    exports: []
+  imports: [TypeOrmModule.forFeature([Tema])],
+  providers: [],
+  controllers: [],
+  exports: [],
 })
 export class TemaModule {}
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,9 +2,11 @@ import { IsNotEmpty } from 'class-validator';
 import {
   Column,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Tema } from '../../Tema/entities/tema.entity';
 
 @Entity({ name: 'tb_postagem' })
 export class Postagem {
@@ -21,4 +23,9 @@ export class Postagem {
 
   @UpdateDateColumn()
   data: Date;
+
+  @ManyToOne(() => Tema, (tema) => tema.Postagem, {
+    onDelete: 'CASCADE',
+  })
+  tema: Tema;
 }
